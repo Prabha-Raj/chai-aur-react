95c93ca7529dd797bc84e2b64b8589e94b1246cc
Prabha <prabhacodingworld@gmail.com>
2024-06-04 04:26:20 +0530

    Counter Project

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+import './App.css'
+
+function App() {
+  /* without useState
+  let counter = 0;
+  // const h2 = document.querySelector('h2')
+  function incrementValue(){
+    counter += 1
+    h2.innerHTML = `${counter}`
+    // console.log(counter)
+  }
+  function decrementValue(){
+    counter -= 1
+    h2.innerHTML = `${counter}`
+    // console.log(counter)
+  }
+  */
+
+  /* using useState */
+  let [counter, setCounter] = useState(0)
+  let [msg, setMsg] = useState('')
+
+  function incrementValue(){
+    if(counter < 10){
+      setMsg(msg = '')
+      setCounter(++counter)
+    }else{
+      setMsg(msg = "You are reached MAXIMUM Limit | to increment value that value is 10")
+    }
+  }
+  function decrementValue(){
+    if(counter > 0){
+      setMsg(msg = '')
+      setCounter(--counter)
+    }else{
+      setMsg(msg = "You are reached MAXIMUM Limit | to decrement value that max value is 0")
+    }
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={incrementValue}>Increment : {counter}</button>
+      &nbsp;&nbsp;
+      <button onClick={decrementValue}>Decrement : {counter}</button>
+      <p>{msg}</p>
+    </>
+  )
+}
+
+export default App
