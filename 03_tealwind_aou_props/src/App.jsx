c87a43fc13108b81bcfbd34ee5b76b0916f661e8
Prabha <prabhacodingworld@gmail.com>
2024-06-05 04:55:29 +0530

    tailwind css and pops(properties)

---
+++ 03_tealwind_aou_props/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+// const myObj = {
+//   username: "Prabha",
+//   Password: "abha@3565"
+// }
+// const myArr = [1, 3, 4, 6,]
+  return (
+    <>
+      <h1 className='mb-4 bg-green-400 text-black p-4 rounded-xl'>Tailwind CSS test</h1>
+      <Card username="chaiaurcode"/>
+    
+    </>
+  )
+}
+
+export default App
